cc531adf723496849334c1dd53b7e5eabcc55e66
swathieshar <118013397+swathi123404@users.noreply.github.com>
2023-02-14 16:57:23 +0530

    first commit

---
+++ Sporty-shoes/src/main/java/com/ss/SportyShoesApplication.java
@@ -0,0 +1,15 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SportyShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesApplication.class, args);
+	}
+
+}
